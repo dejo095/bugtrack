61fb1cf3bdfabafc8fc6a41314e8b69d933dc4b1
Dejan Brnic <dejan.brnic@aerdata.com>
2019-04-05 09:18:10 +0200

    added service to backend

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -1,5 +1,11 @@
 const users = require('./users/users.service.js');
+const products = require('./products/products.service.js');
+const boards = require('./boards/boards.service.js');
+const issues = require('./issues/issues.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
+  app.configure(products);
+  app.configure(boards);
+  app.configure(issues);
 };
